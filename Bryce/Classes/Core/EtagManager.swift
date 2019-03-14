94cc8d6a8c59018ea2c843aaa50b8dad148a26e9
Ephraim Russo <ephraim.s.russo@gmail.com>
2019-03-14 00:33:11 -0700

    Add methods that use DefaultDataResponse and DataResponse<Any> completion handlers.

---
+++ Bryce/Classes/Core/EtagManager.swift
@@ -0,0 +1,84 @@
+//
+//  EtagManager.swift
+//  BryceNetworking
+//
+//  Created by Ephraim Russo on 3/14/19.
+//
+
+import Foundation
+import Alamofire
+
+struct EtagManager {
+    
+    private static let etagHeaderKey = "Etag"
+    
+    private static let etagMapDefaultsKey = "BryceNetworking.EtagMap"
+    
+    private static var _etagMap: [String : String]?
+    
+    private static var etagMap: [String : String]? {
+        
+        get {
+            if let map = _etagMap { return map }
+            else {
+                let map = UserDefaults.standard.value(forKey: etagMapDefaultsKey) as? [String : String]
+                _etagMap = map
+                return map
+            }
+        }
+        
+        set {
+            _etagMap = newValue
+            UserDefaults.standard.set(newValue, forKey: etagMapDefaultsKey)
+        }
+    }
+    
+    private static func setEtag(_ etag: String, for urlString: String) {
+        var map = etagMap ?? [:]
+        map[urlString] = etag
+        etagMap = map
+    }
+    
+    private static func etag(for urlString: String) -> String? {
+        
+        return etagMap?[urlString]
+    }
+    
+    static func headersFrom(endpoint: URLConvertible, method: HTTPMethod, etagEnabled: Bool, headers: HTTPHeaders?) -> HTTPHeaders? {
+        
+        guard method == .get && etagEnabled, let urlString = (try? endpoint.asURL())?.absoluteString else { return headers }
+        
+        var result = headers ?? [:]
+        if let etag = etag(for: urlString) {
+            result[etagHeaderKey] = etag
+        }
+        
+        return result
+    }
+    
+    static func storeEtag(endpoint: URLConvertible, method: HTTPMethod, etagEnabled: Bool, response: DefaultDataResponse) {
+        
+        guard method == .get, etagEnabled == true, let etag = response.response?.allHeaderFields[etagHeaderKey] as? String, let urlString = (try? endpoint.asURL())?.absoluteString else { return }
+        
+        setEtag(etag, for: urlString)
+    }
+    
+    static func storeEtag(endpoint: URLConvertible, method: HTTPMethod, etagEnabled: Bool, response: DataResponse<Any>) {
+        
+        guard method == .get, etagEnabled == true, let etag = response.response?.allHeaderFields[etagHeaderKey] as? String, let urlString = (try? endpoint.asURL())?.absoluteString else { return }
+
+        setEtag(etag, for: urlString)
+    }
+    
+    static func storeEtag<D: Decodable>(endpoint: URLConvertible, method: HTTPMethod, etagEnabled: Bool, response: DataResponse<D>) {
+        
+        guard method == .get, etagEnabled == true, let etag = response.response?.allHeaderFields[etagHeaderKey] as? String, let urlString = (try? endpoint.asURL())?.absoluteString else { return }
+        
+        setEtag(etag, for: urlString)
+    }
+    
+    static func clearEtagMap() {
+        
+        etagMap = nil
+    }
+}
--- Bryce/Classes/HTTP/HTTP.swift
+++ Bryce/Classes/HTTP/HTTP.swift
@@ -9,6 +9,10 @@ import Foundation
 import Alamofire
 import CodableAlamofire
 
+public typealias DefaultAlamofireResponse = (DefaultDataResponse) -> Void
+
+public typealias JSONAlamofireResponse = (DataResponse<Any>) -> Void
+
 public typealias ErrorResponse = (Error?) -> Void
 
 public typealias JSONResponse = (JSON?, Error?) -> Void
@@ -20,15 +24,38 @@ public typealias DecodableResponse<D: Decodable> = (D?, Error?) -> Void
 extension Bryce {
     
     @discardableResult
-    public func request(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, response: @escaping ErrorResponse) -> DataRequest {
+    public func request(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping DefaultAlamofireResponse) -> DataRequest {
         
-        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headers)
+        let headersToSend = EtagManager.headersFrom(endpoint: endpoint, method: method, etagEnabled: etagEnabled, headers: headers)
+        
+        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headersToSend)
+        
+        if validate { dataRequest = dataRequest.validate() }
+        
+        dataRequest.response(queue: self.configuration.responseQueue) { alamofireResponse in
+            
+            EtagManager.storeEtag(endpoint: endpoint, method: method, etagEnabled: etagEnabled, response: alamofireResponse)
+            
+            response(alamofireResponse)
+        }
+        
+        return dataRequest
+    }
+    
+    @discardableResult
+    public func request(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping ErrorResponse) -> DataRequest {
+        
+        let headersToSend = EtagManager.headersFrom(endpoint: endpoint, method: method, etagEnabled: etagEnabled, headers: headers)
+
+        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headersToSend)
             
         if validate { dataRequest = dataRequest.validate() }
             
         dataRequest.response(queue: self.configuration.responseQueue) { alamofireResponse in
             
             let error = alamofireResponse.error
+            
+            EtagManager.storeEtag(endpoint: endpoint, method: method, etagEnabled: etagEnabled, response: alamofireResponse)
             
             response(error)
         }
@@ -37,9 +64,30 @@ extension Bryce {
     }
     
     @discardableResult
-    public func request(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, response: @escaping JSONResponse) -> DataRequest {
+    public func request(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping JSONAlamofireResponse) -> DataRequest {
         
-        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headers)
+        let headersToSend = EtagManager.headersFrom(endpoint: endpoint, method: method, etagEnabled: etagEnabled, headers: headers)
+
+        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headersToSend)
+        
+        if validate { dataRequest = dataRequest.validate() }
+        
+        dataRequest.responseJSON(queue: self.configuration.responseQueue) { alamofireResponse in
+            
+            EtagManager.storeEtag(endpoint: endpoint, method: method, etagEnabled: etagEnabled, response: alamofireResponse)
+            
+            response(alamofireResponse)
+        }
+        
+        return dataRequest
+    }
+    
+    @discardableResult
+    public func request(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping JSONResponse) -> DataRequest {
+        
+        let headersToSend = EtagManager.headersFrom(endpoint: endpoint, method: method, etagEnabled: etagEnabled, headers: headers)
+        
+        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headersToSend)
             
         if validate { dataRequest = dataRequest.validate() }
 
@@ -48,6 +96,8 @@ extension Bryce {
             let json = alamofireResponse.result.value as? [String : Any]
             let error = alamofireResponse.result.error
             
+            EtagManager.storeEtag(endpoint: endpoint, method: method, etagEnabled: etagEnabled, response: alamofireResponse)
+            
             response(json, error)
         }
         
@@ -55,9 +105,11 @@ extension Bryce {
     }
     
     @discardableResult
-    public func request(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, response: @escaping JSONArrayResponse) -> DataRequest {
+    public func request(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping JSONArrayResponse) -> DataRequest {
         
-        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headers)
+        let headersToSend = EtagManager.headersFrom(endpoint: endpoint, method: method, etagEnabled: etagEnabled, headers: headers)
+
+        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headersToSend)
             
         if validate { dataRequest = dataRequest.validate() }
 
@@ -65,6 +117,8 @@ extension Bryce {
             
             let array = alamofireResponse.result.value as? Array<[String : Any]>
             let error = alamofireResponse.result.error
+            
+            EtagManager.storeEtag(endpoint: endpoint, method: method, etagEnabled: etagEnabled, response: alamofireResponse)
             
             response(array, error)
         }
@@ -76,36 +130,38 @@ extension Bryce {
 extension Bryce {
     
     @discardableResult
-    public func request<E: Encodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: E, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, response: @escaping ErrorResponse) -> DataRequest {
+    public func request<E: Encodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: E, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping ErrorResponse) -> DataRequest {
         
         let params = try! parameters.parameters(using: self.configuration.requestEncoder)
         
-        return request(on: endpoint, method: method, parameters: params, encoding: encoding, headers: headers, validate: validate, response: response)
+        return request(on: endpoint, method: method, parameters: params, encoding: encoding, headers: headers, validate: validate, etagEnabled: etagEnabled, response: response)
     }
     
     @discardableResult
-    public func request<E: Encodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: E, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, response: @escaping JSONResponse) -> DataRequest {
+    public func request<E: Encodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: E, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping JSONResponse) -> DataRequest {
         
         let params = try! parameters.parameters(using: self.configuration.requestEncoder)
         
-        return request(on: endpoint, method: method, parameters: params, encoding: encoding, headers: headers, validate: validate, response: response)
+        return request(on: endpoint, method: method, parameters: params, encoding: encoding, headers: headers, validate: validate, etagEnabled: etagEnabled, response: response)
     }
     
     @discardableResult
-    public func request<E: Encodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: E, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, response: @escaping JSONArrayResponse) -> DataRequest {
+    public func request<E: Encodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: E, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping JSONArrayResponse) -> DataRequest {
         
         let params = try! parameters.parameters(using: self.configuration.requestEncoder)
         
-        return request(on: endpoint, method: method, parameters: params, encoding: encoding, headers: headers, validate: validate, response: response)
+        return request(on: endpoint, method: method, parameters: params, encoding: encoding, headers: headers, validate: validate, etagEnabled: etagEnabled, response: response)
     }
 }
 
 extension Bryce {
     
     @discardableResult
-    public func request<D: Decodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, response: @escaping DecodableResponse<D>) -> DataRequest {
+    public func request<D: Decodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping DecodableResponse<D>) -> DataRequest {
         
-        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headers)
+        let headersToSend = EtagManager.headersFrom(endpoint: endpoint, method: method, etagEnabled: etagEnabled, headers: headers)
+        
+        var dataRequest = self.configuration.sessionManager.request(endpoint, method: method, parameters: parameters, encoding: encoding, headers: headersToSend)
             
         if validate { dataRequest = dataRequest.validate() }
 
@@ -114,6 +170,8 @@ extension Bryce {
             let decodable = alamofireResponse.result.value
             let error = alamofireResponse.result.error
             
+            EtagManager.storeEtag(endpoint: endpoint, method: method, etagEnabled: etagEnabled, response: alamofireResponse)
+            
             response(decodable, error)
         }
         
@@ -121,10 +179,10 @@ extension Bryce {
     }
     
     @discardableResult
-    public func request<E: Encodable, D: Decodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: E, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, response: @escaping DecodableResponse<D>) -> DataRequest {
+    public func request<E: Encodable, D: Decodable>(on endpoint: URLConvertible, method: HTTPMethod = .get, parameters: E, encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, validate: Bool = false, etagEnabled: Bool = false, response: @escaping DecodableResponse<D>) -> DataRequest {
         
         let params = try! parameters.parameters(using: self.configuration.requestEncoder)
         
-        return request(on: endpoint, method: method, parameters: params, encoding: encoding, headers: headers, validate: validate, response: response)
+        return request(on: endpoint, method: method, parameters: params, encoding: encoding, headers: headers, validate: validate, etagEnabled: etagEnabled, response: response)
     }
 }
