c3bf954bd50a61d3f62328edcd8dcc306c671ba3
Ephraim Russo <ephraim.s.russo@gmail.com>
2019-12-26 21:15:08 -0800

    Update Swift Package Manifest

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", .upToNextMajor(from: "4.1.0")),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("4.9.1")),
-        .package(url: "https://github.com/erusso1/AlamofireNetworkActivityLogger.git", .branch("feature/swift-package")),
+        .package(url: "https://github.com/erusso1/AlamofireNetworkActivityLogger.git", .exact("2.4.1")),
         .package(url: "https://github.com/Otbivnoe/CodableAlamofire.git", .exact("1.1.2")),
     ],
     targets: [
